6adef3316eeb9e79bfd6cad1d25a5136a29964db
rodlferreira <rodriggo.lferreira@icloud.com>
2023-08-09 23:14:09 -0300

    terceiro dia de criação do Dashboard

--- src/components/Sidebar/Logo.tsx
+++ src/components/Sidebar/Logo.tsx
@@ -1,7 +1,7 @@
 
 export function Logo() {
     return (
-        <strong className="flex items-center gap-2 text-xl font-semibold text-zinc-900">
+        <strong className="flex mx-1 items-center gap-2 text-xl font-semibold text-zinc-900">
             <svg
                 width={38}
                 height={38}
---
+++ src/components/Sidebar/MainNavigation/NavItem.tsx
@@ -0,0 +1,19 @@
+import { CaretCircleDoubleDown } from "@phosphor-icons/react";
+import { ElementType } from "react"
+
+export interface NavItemProps {
+    title: string;
+    icon: ElementType;
+}
+
+export function NavItem({ title, icon: Icon }: NavItemProps) {
+    return (
+        <nav className="space-y-0.5">
+            <a href="Home" className="group flex items-center gap-3 rounded px-3 py-2 hover:bg-violet-50">
+                <Icon size={32} className="h-5 w-5 text-zinc-500" />
+                <span className="font-medium text-zinc-700 group-hover:text-violet-500">Home</span>
+                <CaretCircleDoubleDown size={32} className="ml-auto h-5 w-5 text-zinc-400 group-hover:text-violet-300" />
+            </a>
+        </nav>
+    )
+}
