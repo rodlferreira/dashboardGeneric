f460e129c428f9b1f8c4a69ba0c8924a7b7acf18
rodlferreira <rodriggo.lferreira@icloud.com>
2023-08-11 01:34:30 +0100

    Terceiro dia de criação do dashboard

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,18 +1,27 @@
 'use client';
-import { MagnifyingGlass } from "@phosphor-icons/react";
+import { GearSix, Lifebuoy, MagnifyingGlass } from "@phosphor-icons/react";
 import { Logo } from "./Logo";
 import { MainNavigation } from "./MainNavigation";
 import { NavItem } from "./MainNavigation/NavItem";
+import { UsedSpaceWidget } from "./MainNavigation/UsedSpaceWidget";
 
 export function Sidebar() {
     return (
-        <aside className="border-r border-zinc-200 px-5 py-8 space-y-6 ">
+        <aside className="border-r border-zinc-200 px-5 py-8 flex flex-col gap-6 ">
             <Logo />
             <div className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
                 <MagnifyingGlass className="h-5 w-5 text-zinc-500" />
                 <input className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600" placeholder="Search" />
             </div>
-                <MainNavigation />
+            <MainNavigation />
+
+            <div className="mt-auto flex flex-col gap-6">
+                <nav className="space-y-0.5">
+                    <NavItem title="Suport" icon={Lifebuoy} />
+                    <NavItem title="Settings" icon={GearSix} />
+                </nav>
+                <UsedSpaceWidget />
+            </div>
         </aside>
     )
 }
