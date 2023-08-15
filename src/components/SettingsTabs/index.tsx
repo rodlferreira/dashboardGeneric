e88e7559995d613ab85f01ba17a6288392204108
rodlferreira <rodriggo.lferreira@icloud.com>
2023-08-16 00:57:36 +0100

    sétimo dia de desenvolvimento do Dashboard

--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -14,7 +14,7 @@ export function SettingsTabs() {
                 <TabItem value="tab2" title="Profile" isSelected={currentTab === 'tab2'} />
                 <TabItem value="tab3" title="Password" isSelected={currentTab === 'tab3'} />
                 <TabItem value="tab4" title="Team" isSelected={currentTab === 'tab4'} />
-                <TabItem value="tab5" title="Plan" isSelected={currentTab === 'ta5'} />
+                <TabItem value="tab5" title="Plan" isSelected={currentTab === 'tab5'} />
                 <TabItem value="tab6" title="Billing" isSelected={currentTab === 'tab6'} />
                 <TabItem value="tab7" title="Email" isSelected={currentTab === 'tab7'} />
                 <TabItem value="tab8" title="Notifications" isSelected={currentTab === 'tab8'} />
