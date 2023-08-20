bc53f0b37f729f0ad76b0e8ee2e3aa4ad635fefb
rodlferreira <rodriggo.lferreira@icloud.com>
2023-08-20 02:37:48 +0100

    décimo primeiro dia de desonvolvimento do Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { SettingsTabs } from "@/components/SettingsTabs";
 import { InputRoot, InputControl, InputPrefix } from "@/components/Input";
-import { Code, Envelope, GlobeStand } from "@phosphor-icons/react";
+import { CloudArrowUp, Code, Envelope, GlobeStand, UserFocus } from "@phosphor-icons/react";
 
 
 export default function Home() {
@@ -33,7 +33,7 @@ export default function Home() {
           </div>
 
         </div>
-        
+
         <form id="Settings" className="mt-6 flex flex-col w-full gap-5 divide-y divide-zinc-300">
           <div className="grid grid-cols-form gap-3">
             <label
@@ -71,7 +71,7 @@ export default function Home() {
 
           <div className="grid grid-cols-form gap-3 pt-5">
             <label
-              htmlFor="email"
+              htmlFor="photo"
               className="text-sm font-medium text-zinc-700"
             >
               Your photo
@@ -81,7 +81,32 @@ export default function Home() {
               </span>
             </label>
 
-            <div></div>
+            <div className="flex items-start gap-5">
+              <div className="bg-violet-100 flex h-16 w-16 items-center justify-center rounded-full">
+                < UserFocus className="w-8 h-8 text-violet-500" />
+              </div>
+
+              <label
+                htmlFor="photo"
+                className=" group flex-1 cursor-pointer flex flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500">
+                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
+                  <CloudArrowUp className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                  <span className="text-sm">
+                    <span className="font-semibold text-violet-700">
+                      Click to upload
+                    </span>
+                    or drag and drop
+                  </span>
+                  <span className="text-xs">
+                    SVG, PNG JPG, or GIF(max. 800x400px)
+                  </span>
+                </div>
+              </label>
+
+              <input type="file" className="sr-only" id="photo" />
+            </div>
 
           </div>
 
