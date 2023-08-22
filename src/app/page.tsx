36e66ac9dfc7226d738d739402c6f15cc6af3c03
rodlferreira <rodriggo.lferreira@icloud.com>
2023-08-22 01:53:25 +0100

    décimo segundo dia de desenvolvimento do Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 import { SettingsTabs } from "@/components/SettingsTabs";
 import { InputRoot, InputControl, InputPrefix } from "@/components/Input";
-import { CloudArrowUp, Code, Envelope, GlobeStand, UserFocus } from "@phosphor-icons/react";
-
+import { Code, Envelope, GlobeStand } from "@phosphor-icons/react";
+import * as FileInput from '@/components/Form/FileInput'
 
 export default function Home() {
   return (
@@ -77,36 +77,15 @@ export default function Home() {
               Your photo
               <span
                 className="text-sm font-normal text-zinc-500 block mt-0.5">
-                Share a few snippets of your work.
+                This will be displayed on your profile.
               </span>
             </label>
 
-            <div className="flex items-start gap-5">
-              <div className="bg-violet-100 flex h-16 w-16 items-center justify-center rounded-full">
-                < UserFocus className="w-8 h-8 text-violet-500" />
-              </div>
-
-              <label
-                htmlFor="photo"
-                className=" group flex-1 cursor-pointer flex flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500">
-                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
-                  <CloudArrowUp className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
-                </div>
-                <div className="flex flex-col items-center gap-1">
-                  <span className="text-sm">
-                    <span className="font-semibold text-violet-700">
-                      Click to upload
-                    </span>
-                    or drag and drop
-                  </span>
-                  <span className="text-xs">
-                    SVG, PNG JPG, or GIF(max. 800x400px)
-                  </span>
-                </div>
-              </label>
-
-              <input type="file" className="sr-only" id="photo" />
-            </div>
+            <FileInput.Root className="flex items-start gap-5">
+              <FileInput.ImagePreview />
+              <FileInput.Trigger />
+              <FileInput.Control />
+            </FileInput.Root>
 
           </div>
 
@@ -168,8 +147,10 @@ export default function Home() {
               </span>
             </label>
 
-            <div></div>
-
+            <FileInput.Root>
+              <FileInput.Trigger />
+              <FileInput.Control multiple/>
+            </FileInput.Root>
           </div>
 
           <div className="flex items-center justify-end gap-2 pt-5">
---
+++ src/components/Form/FileInput/ImagePreview.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { UserFocus } from "@phosphor-icons/react";
+import { useFileInput } from "./Root";
+import { useMemo } from "react";
+
+
+export function ImagePreview() {
+
+  const { files } = useFileInput()
+
+  const previewURL = useMemo(() => {
+
+    if (files.length === 0) {
+      return null
+    }
+
+    return URL.createObjectURL(files[0])
+
+  }, [files])
+
+  if (previewURL === null) {
+    return (
+      <div className="bg-violet-100 flex h-16 w-16 items-center justify-center rounded-full">
+        < UserFocus className="w-8 h-8 text-violet-500" />
+      </div>
+    )
+
+  } else {
+    return (
+      <img src={previewURL} alt="" className="h-16 w-16 rounded-full object-cover" />
+    )
+  }
+
+
+}
