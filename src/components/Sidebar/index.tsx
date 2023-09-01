ac37a7d24bc9606d9468a36ed8ecbb16036481e0
rodlferreira <rodriggo.lferreira@icloud.com>
2023-09-02 00:53:36 +0100

    vigésimo dia de desenvolvimento do Dashboard

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -9,7 +9,7 @@ import { InputRoot, InputControl, InputPrefix } from "../Input";
 
 export function Sidebar() {
     return (
-        <aside className="border-r border-zinc-200 px-5 py-8 flex flex-col gap-6 ">
+        <aside className="border-b border-zinc-200 p-4 bottom-0 flex flex-col gap-6 fixed left-0 top-0 right-0 z-20 bg-white lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:relative">
             <Logo />
             <InputRoot>
                 <InputPrefix>
