4a423d6f22aa402561f0ec2bbdb5905fdce426e7
rodlferreira <rodriggo.lferreira@icloud.com>
2023-09-03 00:24:45 +0100

    vigésimo primeiro dia de desenvolvimento do Dashboard

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,33 +1,45 @@
 'use client';
-import { GearSix, Lifebuoy, MagnifyingGlass } from "@phosphor-icons/react";
+import { DotsThreeOutline, GearSix, Lifebuoy, MagnifyingGlass } from "@phosphor-icons/react";
 import { Logo } from "./Logo";
 import { MainNavigation } from "./MainNavigation";
 import { NavItem } from "./MainNavigation/NavItem";
 import { UsedSpaceWidget } from "./MainNavigation/UsedSpaceWidget";
 import { Profile } from "./Profile";
 import { InputRoot, InputControl, InputPrefix } from "../Input";
+import * as Collpasible from '@radix-ui/react-collapsible'
+import { Button } from "../Button";
 
 export function Sidebar() {
     return (
-        <aside className="border-b border-zinc-200 p-4 bottom-0 flex flex-col gap-6 fixed left-0 top-0 right-0 z-20 bg-white lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:relative">
-            <Logo />
-            <InputRoot>
-                <InputPrefix>
-                    <MagnifyingGlass className="h-5 w-5 text-zinc-500" />
-                </InputPrefix>
-                <InputControl placeholder="Search" />
-            </InputRoot>
-            <MainNavigation />
-
-            <div className="mt-auto flex flex-col gap-6">
-                <nav className="space-y-0.5">
-                    <NavItem title="Suport" icon={Lifebuoy} />
-                    <NavItem title="Settings" icon={GearSix} />
-                </nav>
-                <UsedSpaceWidget />
-                <div className="h-px bg-zinc-200 " />
-                <Profile />
+        <Collpasible.Root className=" first-letter: border-b border-zinc-200 p-4  data-[state=open]:bottom-0  lg:data-[state=closed]:bottom-0 flex flex-col gap-6 fixed left-0 top-0 right-0 z-20 bg-white lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:relative">
+            <div className="flex items-center justify-between">
+                <Logo />
+                <Collpasible.Trigger asChild className="lg:hidden">
+                        <Button variant="ghost">
+                            <DotsThreeOutline className="h-6 w-6"/>
+                        </Button>
+                </Collpasible.Trigger>
             </div>
-        </aside>
+
+            <Collpasible.Content forceMount className="flex-1 flex flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex">
+                <InputRoot>
+                    <InputPrefix>
+                        <MagnifyingGlass className="h-5 w-5 text-zinc-500" />
+                    </InputPrefix>
+                    <InputControl placeholder="Search" />
+                </InputRoot>
+                <MainNavigation />
+
+                <div className="mt-auto flex flex-col gap-6">
+                    <nav className="space-y-0.5">
+                        <NavItem title="Suport" icon={Lifebuoy} />
+                        <NavItem title="Settings" icon={GearSix} />
+                    </nav>
+                    <UsedSpaceWidget />
+                    <div className="h-px bg-zinc-200 " />
+                    <Profile />
+                </div>
+            </Collpasible.Content>
+        </Collpasible.Root>
     )
 }
